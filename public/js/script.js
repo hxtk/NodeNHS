f413e82bd3c5fe86af91ffc2d1f5611fb42ad1e4
peter <edistojr@gmail.com>
2015-09-30 23:56:32 -0400

    last search fix

--- public/js/script.js
+++ public/js/script.js
@@ -6,14 +6,16 @@ function ready(fn) {
     }
 }
 
+function reheight(el,h){
+    style.height = h;
+}
+
 ready(function() {
     document.querySelector("#search input").onfocus = function () {
         this.parentNode.style.height = 'auto';
     };
     document.querySelector("#search input").onblur = function () {
-        var hei = this.parentNode.style.height;
-        window.setTimeout(function(hei){
-            hei = '46px';
-        },5);
+        var hei = this.parentNode.
+        window.setTimeout(reheight(this.parentNode,'46px'),5);
     };
 });
