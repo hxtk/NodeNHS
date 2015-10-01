a6b6221fbfe6338c31060666c3c968e74e67b8df
peter <edistojr@gmail.com>
2015-10-01 10:24:10 -0400

    Search bar fix (again)

--- public/js/script.js
+++ public/js/script.js
@@ -13,8 +13,15 @@ function reheight(el,h){
 ready(function() {
     document.querySelector("#search input").onfocus = function () {
         this.parentNode.style.height = 'auto';
-    };
+    };/*
     document.querySelector("#search input").onblur = function () {
         window.setTimeout(5000,reheight(this.parentNode,'46px'));
+    };*/
+    document.getElementById('search').onclick = function(){
+        return false;
     };
+    document.body.onclick = function(){
+        reheight(document.getElementbyId("search"),'46px');
+    };
+
 });
